178418cb4c995bda67467b5d9db105790eb7e56e
Bryan Garaventa <bryan.garaventa@whatsock.com>
2021-04-14 13:11:47 -0700

    Added help docs for Buttons, Checkboxes, Radios, and Switches.

--- Templates/Switches/Standard/js/setup.js
+++ Templates/Switches/Standard/js/setup.js
@@ -4,8 +4,8 @@ $A.import(["Animate", "Switch"], { defer: true }, function() {
     {
       onActivate: function(ev, triggerNode, boundCheckbox, on, set) {
         // 'on' reflects the current attribute value for the checkable item, and is always a number if applicable.
-        // if 0, the checked state is "false".
-        // if 1, the checked state is "true".
+        // if 0, the toggle state is "false".
+        // if 1, the toggle state is "true".
         // The 'set' argument is a function that will set the checkable item to a new state.
         // The new value must be a string consisting of "false" or "true".
         if (on) {
