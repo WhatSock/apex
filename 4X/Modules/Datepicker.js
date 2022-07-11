cf2bb921a7c4241692ead8520782d01e8ca2fc58
Bryan Garaventa <1865064+accdc@users.noreply.github.com>
2022-07-11 11:55:03 -0700

    Revert "Fix datepicker date parsing for day-first dates"

--- 4X/Modules/Datepicker.js
+++ 4X/Modules/Datepicker.js
@@ -908,15 +908,8 @@ License: MIT <https://opensource.org/licenses/MIT>
                 dc.rerendering = false;
               },
               beforeRender: function(dc) {
-                let dateValue = targ.value;
-                const dateParts = targ.value.split('/');
-                if (config.inputDateFormat === 'DD/MM/YYYY' && dateParts.length === 3) {
-                  dateValue = `${dateParts[1]}/${dateParts[0]}/${dateParts[2]}`;
-                } else if (config.inputDateFormat === 'DD/MM/YYYY') {
-                  dateValue = new Date();
-                }
-                if (!dc.rerendering && dateValue) {
-                  dc.presetDate(dc, new Date(dateValue));
+                if (!dc.rerendering && targ.value) {
+                  dc.presetDate(dc, new Date(targ.value));
                 }
 
                 // based on config option, disable weekdays?
