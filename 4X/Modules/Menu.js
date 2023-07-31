3e7fa1bf4e5e96cb4b62c3f9dbd92ef18515d234
Bryan Garaventa <bryan.garaventa@whatsock.com>
2023-07-30 23:32:53 -0700

    Updated the Menu module to properly support autoCloseSameWidget so that context menus will close automatically when unrelated menus are opened elsewhere on the same page.

--- 4X/Modules/Menu.js
+++ 4X/Modules/Menu.js
@@ -1,5 +1,5 @@
 /*@license
-ARIA Menu Module 3.2 for Apex 4X
+ARIA Menu Module 3.3 for Apex 4X
 Author: Bryan Garaventa (https://www.linkedin.com/in/bgaraventa)
 Home: WhatSock.com  :  Download: https://github.com/whatsock/apex
 License: MIT (https://opensource.org/licenses/MIT)
@@ -14,7 +14,7 @@ Required dependencies: RovingTabIndex.js
       props: props,
       once: true,
       call: function (props) {
-        $A.addWidgetProfile("Menu", {
+        var menuPfl = {
           configure: function (dc) {
             return {
               horizontalHelpTip:
@@ -63,7 +63,9 @@ Required dependencies: RovingTabIndex.js
               "map",
             );
           },
-        });
+        };
+        $A.addWidgetProfile("Menu", menuPfl);
+        $A.addWidgetProfile("SubMenu", menuPfl);
 
         var isIE = $A.isIE();
 
@@ -137,7 +139,7 @@ Required dependencies: RovingTabIndex.js
                 }
                 return false;
               },
-              genMenu = function (o, p, list) {
+              genMenu = function (o, p, list, isTopMenu) {
                 if (!$A.isNode(o)) return;
                 var ref =
                   list ||
@@ -158,8 +160,8 @@ Required dependencies: RovingTabIndex.js
                       trigger: o,
                       content: ref,
                       on: "openmenu",
-                      widgetType: "Menu",
-                      autoCloseSameWidget: false,
+                      widgetType: isTopMenu ? "Menu" : "SubMenu",
+                      autoCloseSameWidget: isTopMenu ? true : false,
                       toggleHide: true,
                       getState: getState,
                     },
@@ -335,7 +337,7 @@ Required dependencies: RovingTabIndex.js
 
             $A.query(o, config.context || document, function (i, o) {
               var gen = function (m) {
-                DC = genMenu(o, null, m);
+                DC = genMenu(o, null, m, true);
                 $A.on(window.document, "click.closemenus", function () {
                   DC.top.remove();
                 });
