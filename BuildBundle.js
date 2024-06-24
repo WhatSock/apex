7a434275e8e4708ec0524aa501ef81f6a6dcdfb0
Bryan Garaventa <bryan.garaventa@whatsock.com>
2024-06-24 14:21:12 -0700

    Updated the BuildBundle.js file to include references to all dependencies to make editing easier.

--- BuildBundle.js
+++ BuildBundle.js
@@ -1,31 +1,31 @@
-import "./4X/4X.Max.Export.js";
-import "./4X/Standard/Modules/CurrentDevice.js";
-import "./4X/Standard/Modules/Dragdealer.js";
-import "./4X/Standard/Modules/Dragula.js";
-import "./4X/Standard/Modules/TinySlider.js";
-import "./4X/Standard/Modules/Velocity.js";
-import "./4X/Standard/Modules/VelocityUI.js";
-import "./4X/Standard/Modules/AccName.js";
-import "./4X/Standard/Modules/Animate.js";
-import "./4X/Standard/Modules/RovingTabIndex.js";
-import "./4X/Standard/Modules/SmoothScroll.js";
-import "./4X/Standard/Modules/Accordion.js";
-import "./4X/Standard/Modules/Beep.js";
-import "./4X/Standard/Modules/Button.js";
-import "./4X/Standard/Modules/Carousel.js";
-import "./4X/Standard/Modules/Combobox.js";
-import "./4X/Standard/Modules/Datepicker.js";
-import "./4X/Standard/Modules/Dialog.js";
-import "./4X/Standard/Modules/Drag.js";
-import "./4X/Standard/Modules/Footnote.js";
-import "./4X/Standard/Modules/Grid.js";
-import "./4X/Standard/Modules/Listbox.js";
-import "./4X/Standard/Modules/Menu.js";
-import "./4X/Standard/Modules/Popup.js";
-import "./4X/Standard/Modules/Slider.js";
-import "./4X/Standard/Modules/Tab.js";
-import "./4X/Standard/Modules/Tooltip.js";
-import "./4X/Standard/Modules/Tree.js";
-import "./4X/Standard/Modules/Straylight.js";
-import "./4X/Standard/Modules/Dragula.css";
-import "./4X/Standard/Modules/TinySlider.css";
+import "./4X/4X.Max.Export.js"; // Core 4X API, REQUIRED.
+import "./4X/Standard/Modules/CurrentDevice.js"; // Has no dependencies; is common dependency of other modules.
+import "./4X/Standard/Modules/Dragdealer.js"; // Has no dependencies; is common dependency of other modules.
+import "./4X/Standard/Modules/Dragula.js"; // Has no dependencies; is common dependency of other modules.
+import "./4X/Standard/Modules/TinySlider.js"; // Has no dependencies; is common dependency of other modules.
+import "./4X/Standard/Modules/Velocity.js"; // Has no dependencies; is common dependency of other modules.
+import "./4X/Standard/Modules/VelocityUI.js"; // Requires Velocity.js; is common dependency of other modules.
+import "./4X/Standard/Modules/AccName.js"; // Has no dependencies; is common dependency of other modules.
+import "./4X/Standard/Modules/Animate.js"; // Requires Velocity.js, VelocityUI.js; is common dependency of other modules.
+import "./4X/Standard/Modules/RovingTabIndex.js"; // Requires AccName.js; is common dependency of other modules.
+import "./4X/Standard/Modules/SmoothScroll.js"; // Requires Animate.js; is common dependency of other modules.
+import "./4X/Standard/Modules/Accordion.js"; // Requires RovingTabIndex.js.
+import "./4X/Standard/Modules/Beep.js"; // Has no dependencies.
+import "./4X/Standard/Modules/Button.js"; // Requires RovingTabIndex.js.
+import "./4X/Standard/Modules/Carousel.js"; // Requires TinySlider.css, TinySlider.js.
+import "./4X/Standard/Modules/Combobox.js"; // Requires SmoothScroll.js, AccName.js, CurrentDevice.js.
+import "./4X/Standard/Modules/Datepicker.js"; // Has no dependencies.
+import "./4X/Standard/Modules/Dialog.js"; // Has no dependencies.
+import "./4X/Standard/Modules/Drag.js"; // Requires Animate.js, Menu.js, Dragula.css, Dragula.js; is common dependency of other modules.
+import "./4X/Standard/Modules/Footnote.js"; // Requires SmoothScroll.js.
+import "./4X/Standard/Modules/Grid.js"; // Has no dependencies.
+import "./4X/Standard/Modules/Listbox.js"; // Requires RovingTabIndex.js.
+import "./4X/Standard/Modules/Menu.js"; // Requires RovingTabIndex.js.
+import "./4X/Standard/Modules/Popup.js"; // Has no dependencies.
+import "./4X/Standard/Modules/Slider.js"; // Requires CurrentDevice.js, Dragdealer.js.
+import "./4X/Standard/Modules/Tab.js"; // Requires RovingTabIndex.js.
+import "./4X/Standard/Modules/Tooltip.js"; // Has no dependencies.
+import "./4X/Standard/Modules/Tree.js"; // Requires RovingTabIndex.js.
+import "./4X/Standard/Modules/Straylight.js"; // Requires all other widget modules.
+import "./4X/Standard/Modules/Dragula.css"; // Requires Dragula.js.
+import "./4X/Standard/Modules/TinySlider.css"; // Requires TinySlider.js.
