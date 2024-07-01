8148da8f9173dc5a811421972ab9ca577beba610
Bryan Garaventa <bryan.garaventa@whatsock.com>
2024-07-01 09:14:24 -0700

    Updated the React create script to ensure it only runs once, plus added a start server script.

--- React/NPM Start.sh
+++ React/NPM Start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 if [ ! -d "./test-react-app/src" ]; then
-npx create-react-app test-react-app
-cp -r "./src"/* "./test-react-app/src"
+  exit 1
+fi
 cd test-react-app
 npm install apex4x
 npm start
-fi
