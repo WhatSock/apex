82b3aaabdc0665ae3b18065da33f9b922477dbbc
Bryan Garaventa <bryan.garaventa@whatsock.com>
2024-06-30 17:44:15 -0700

    Updated the Button module to add setToggle as an alias for setButton.

--- 4X/Modules/Button.js
+++ 4X/Modules/Button.js
@@ -1,5 +1,5 @@
 /*@license
-ARIA Button Module 1.0 for Apex 4X
+ARIA Button Module 1.1 for Apex 4X
 Author: Bryan Garaventa (https://www.linkedin.com/in/bgaraventa)
 Home: WhatSock.com  :  Download: https://github.com/whatsock/apex
 License: MIT (https://opensource.org/licenses/MIT)
@@ -374,6 +374,7 @@ Required dependencies: RovingTabIndex.js
           setCheckbox: $A["setButton"],
           setRadio: $A["setButton"],
           setSwitch: $A["setButton"],
+          setToggle: $A["setButton"],
         });
       },
     });
